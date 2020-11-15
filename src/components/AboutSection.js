ca64aa294e820616f7d3d9f53d14673494ec1741
Nelo Puchades <correo@nelo.dev>
2020-11-15 22:29:30 +0100

    Add custom animations to about page

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,4 +1,5 @@
 import { motion } from "framer-motion";
+import { titleAnimation, fadeAnimation, photoAnimation } from "../animation";
 import {
   AboutContainer,
   DescriptionContainer,
@@ -13,25 +14,29 @@ const AboutSection = () => {
       <DescriptionContainer>
         <motion.div>
           <Hide>
-            <motion.h2>We work to make</motion.h2>
+            <motion.h2 variants={titleAnimation}>We work to make</motion.h2>
           </Hide>
           <Hide>
-            <motion.h2>
+            <motion.h2 variants={titleAnimation}>
               your <span>dreams</span>
             </motion.h2>
           </Hide>
           <Hide>
-            <motion.h2>come true.</motion.h2>
+            <motion.h2 variants={titleAnimation}>come true.</motion.h2>
           </Hide>
         </motion.div>
-        <p>
+        <motion.p variants={fadeAnimation}>
           Contact us for any photography or videography ideas that you have. We
           have professionals with amazing skills.
-        </p>
-        <button>Contact Us</button>
+        </motion.p>
+        <motion.button variants={fadeAnimation}>Contact Us</motion.button>
       </DescriptionContainer>
       <ImageContainer>
-        <img src={home1} alt="Guy with a camera" />
+        <motion.img
+          variants={photoAnimation}
+          src={home1}
+          alt="Guy with a camera"
+        />
       </ImageContainer>
     </AboutContainer>
   );
