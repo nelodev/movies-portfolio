459b406d78da459f060b2de7cf1a828547dce556
Nelo Puchades <correo@nelo.dev>
2020-11-15 23:00:31 +0100

    Add animations to our work page

--- src/pages/OurWork.js
+++ src/pages/OurWork.js
@@ -1,7 +1,14 @@
+import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 import styled from "styled-components";
-import { pageAnimation } from "../animation";
-import { Link } from "react-router-dom";
+import {
+  pageAnimation,
+  fadeAnimation,
+  photoAnimation,
+  lineAnimation,
+  sliderAnimation,
+  sliderContainer,
+} from "../animation";
 import athlete from "../img/athlete-small.png";
 import theracer from "../img/theracer-small.png";
 import goodtimes from "../img/goodtimes-small.png";
@@ -19,7 +26,7 @@ const MovieContainer = styled.div`
   padding-bottom: 10rem;
   .line {
     height: 0.5rem;
-    background-color: #cccccc;
+    background-color: #23d997;
     margin-bottom: 3rem;
   }
   img {
@@ -27,6 +34,32 @@ const MovieContainer = styled.div`
     height: 70vh;
     object-fit: cover;
   }
+`;
+
+const Hide = styled.div`
+  overflow: hidden;
+`;
+
+const Frame1 = styled(motion.div)`
+  position: fixed;
+  left: 0;
+  top: 10%;
+  width: 100%;
+  height: 100vh;
+  background: #fffebf;
+  z-index: 2;
+`;
+
+const Frame2 = styled(Frame1)`
+  background: #ff8efb;
+`;
+
+const Frame3 = styled(Frame1)`
+  background: #8ed2ff;
+`;
+
+const Frame4 = styled(Frame1)`
+  background: #8effa0;
 `;
 
 const OurWork = () => {
@@ -38,11 +71,19 @@ const OurWork = () => {
       animate="show"
       exit="exit"
     >
+      <motion.div variants={sliderContainer}>
+        <Frame1 variants={sliderAnimation} />
+        <Frame2 variants={sliderAnimation} />
+        <Frame3 variants={sliderAnimation} />
+        <Frame4 variants={sliderAnimation} />
+      </motion.div>
       <MovieContainer>
-        <h2>The Athlete</h2>
-        <div className="line"></div>
+        <motion.h2 variants={fadeAnimation}>The Athlete</motion.h2>
+        <motion.div variants={lineAnimation} className="line"></motion.div>
         <Link to="/our-work/the-athlete">
-          <img src={athlete} alt="Athlete" />
+          <Hide>
+            <motion.img variants={photoAnimation} src={athlete} alt="Athlete" />
+          </Hide>
         </Link>
       </MovieContainer>
       <MovieContainer>
