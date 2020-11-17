3b25933219549895fa8fdf762e408db6b1bfc47a
Nelo Puchades <correo@nelo.dev>
2020-11-17 21:20:31 +0100

    Added animation to toggle action in About Us section

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,5 +1,7 @@
 import styled from "styled-components";
+import { AnimateSharedLayout } from "framer-motion";
 import { AboutContainer } from "../styles";
+import Toggle from "./Toggle";
 
 const FAQContainer = styled(AboutContainer)`
   display: block;
@@ -34,46 +36,40 @@ const FaqSection = () => {
       <h2>
         Any Questions? <span>FAQ</span>
       </h2>
-      <div className="question">
-        <h4>How Do I Start?</h4>
-        <div className="answer">
-          <p>
-            Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet consectetur,
-            adipisicing elit. Accusantium, recusandae?
-          </p>
-        </div>
-        <div className="faq-line"></div>
-      </div>
-      <div className="question">
-        <h4>Daily Schedule</h4>
-        <div className="answer">
-          <p>
-            Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet consectetur,
-            adipisicing elit. Accusantium, recusandae?
-          </p>
-        </div>
-        <div className="faq-line"></div>
-      </div>
-      <div className="question">
-        <h4>Different Payment Methods</h4>
-        <div className="answer">
-          <p>
-            Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet consectetur,
-            adipisicing elit. Accusantium, recusandae?
-          </p>
-        </div>
-        <div className="faq-line"></div>
-      </div>
-      <div className="question">
-        <h4>What products do you offer?</h4>
-        <div className="answer">
-          <p>
-            Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet consectetur,
-            adipisicing elit. Accusantium, recusandae?
-          </p>
-        </div>
-        <div className="faq-line"></div>
-      </div>
+      <AnimateSharedLayout>
+        <Toggle title="How Do I Start?">
+          <div className="answer">
+            <p>
+              Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet
+              consectetur, adipisicing elit. Accusantium, recusandae?
+            </p>
+          </div>
+        </Toggle>
+        <Toggle title="Daily Schedule">
+          <div className="answer">
+            <p>
+              Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet
+              consectetur, adipisicing elit. Accusantium, recusandae?
+            </p>
+          </div>
+        </Toggle>
+        <Toggle title="Different Payment Methods">
+          <div className="answer">
+            <p>
+              Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet
+              consectetur, adipisicing elit. Accusantium, recusandae?
+            </p>
+          </div>
+        </Toggle>
+        <Toggle title="What products do you offer?">
+          <div className="answer">
+            <p>
+              Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet
+              consectetur, adipisicing elit. Accusantium, recusandae?
+            </p>
+          </div>
+        </Toggle>
+      </AnimateSharedLayout>
     </FAQContainer>
   );
 };
