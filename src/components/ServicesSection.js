6e30d48ce9d3adefaea01430a80a2ebd6a1b1afe
Nelo Puchades <correo@nelo.dev>
2020-11-17 23:16:23 +0100

    First version of responsive

--- src/components/ServicesSection.js
+++ src/components/ServicesSection.js
@@ -26,6 +26,9 @@ const ServicesContainer = styled(AboutContainer)`
 const CardsContainer = styled.div`
   display: flex;
   flex-wrap: wrap;
+  @media (max-width: 1300px) {
+    justify-content: center;
+  }
 `;
 
 const Card = styled.div`
